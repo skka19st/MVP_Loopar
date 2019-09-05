d80edb3b015a03a373702e1959f34faf3595caed
Karin Skans <skka19st@student.ju.se>
2019-09-05 11:06:01 +0200

    version klar fÃor granskning

--- script.js
+++ script.js
@@ -28,78 +28,40 @@ function ListaStart()
     console.log("ListaStart");
 }
 
-// funktion ListaItem, skapar list-rader
+// funktion ListaItem, skapar list-rader (html-kod)
 function ListaItem()
 {
-    // raderna som skapas här ska kopplas till list-taggen
+    // raderna som skapas här ska kopplas till ul-taggen
     // koppling via TagName kräver indexering (ej unikt)
     let ulTag = document.getElementsByTagName("ul")[0];
-    //var text = "";
 
+    // skapa en tom li-tag för varje rad i arrayen
+    for (let ind = 0 ; ind<roligaSaker.length ; ind++)
+    {
+        ulTag.insertAdjacentHTML("afterend" ,"<li></li>");    
+
+        console.log("<li>" + roligaSaker[ind] + "</li>");
+    }
+}
+
+// funktion ListaItemContent, skapar innehåll i li-taggarna
+function ListaItemContent()
+{
     // innehållet i arrayen läggs ut till skärmen
     for (let ind = 0 ; ind<roligaSaker.length ; ind++)
     {
-        ulTag.insertAdjacentHTML("afterend" 
-        ,"<li> li-taggen </li>");    
-        
         // koppling via TagName kräver indexering (ej unikt)
         let liTag = document.getElementsByTagName("li")[ind];
 
         // arrayraden läggs ut till den listrad som skapas
         var radText = document.createTextNode(roligaSaker[ind]);
         liTag.appendChild(radText);
-
-        //ulTag.insertAdjacentHTML("beforeend" ,"</li>");
-
-        //text = text+("<li>" + roligaSaker[ind] + "</li>");
-        console.log("<li>" + roligaSaker[ind] + "</li>");
     }
-
-    // innehållet i arrayen läggs ut till skärmen
-    for (let ind = 0 ; ind<roligaSaker.length ; ind++)
-    {
-        ulTag.insertAdjacentHTML("afterend" 
-        ,"<li> li-taggen </li>");    
-        
-        // koppling via TagName kräver indexering (ej unikt)
-        let liTag = document.getElementsByTagName("li")[ind];
-
-        // arrayraden läggs ut till den listrad som skapas
-        var radText = document.createTextNode(roligaSaker[ind]);
-        liTag.appendChild(radText);
-
-        //ulTag.insertAdjacentHTML("beforeend" ,"</li>");
-
-        //text = text+("<li>" + roligaSaker[ind] + "</li>");
-        console.log("<li>" + roligaSaker[ind] + "</li>");
-    }
-
-    // den skapade html-raden som under ul-taggen
-    //liTag.appendChild(text);
-    //console.log(text);
 }
 
 // funktion ListaStopp, skapar stopp-taggen
 function ListaStopp()
 {
-        // skapa en onumrerad lista
-        //var startTag = document.createElement("/ul");
-
-        // lägg text i listans rubrik
-        // skapa en textrad och koppla den till div-taggen
-        //var text = document.createTextNode("Listans rubrik");
-        //divTag.appendChild(text);
         divTag.insertAdjacentHTML("beforeend" ,"</li>");
         console.log("ListaStopp");
 }
-
-// // skriv ut innehållet i arrayen till div-taggen
-// for (let ind = 0 ; ind<roligaSaker.length ; ind++)
-// {
-//     //divTagArray.insertAdjacentText('afterbegin','33');
-//     // textRadDiv = document.createTextNode(braSaker[ind]);
-//     // divTag.appendChild(textRadDiv);
-
-//     console.log(roligaSaker[ind]);             // skriver ut till loggen
-// }
-//--------------------------------------------------------------------
